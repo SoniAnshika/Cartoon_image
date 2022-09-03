189c68fb38ca45b2bcba32681d70591c8a1edc79
Anshika Soni <91690050+SoniAnshika@users.noreply.github.com>
2022-09-04 00:17:10 +0530

    Added Code of the model

---
+++ cartoon.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+def read_img(filename):
+    img=cv2.imread(filename)
+    return img
+
+def edge_detection(img,line_wdt,blur):
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    grayBlur=cv2.medianBlur(gray,blur)
+    edges=cv2.adaptiveThreshold(grayBlur,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,line_wdt,blur)
+    return edges
+
+def color_quantisation(img,k):
+    data=np.float32(img).reshape((-1,3))
+    criteria=(cv2.TermCriteria_EPS + cv2.TermCriteria_MAX_ITER,20,0.001)
+    ret, label, center=cv2.kmeans(data,k,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    center=np.uint8(center)
+    result=center[label.flatten()]
+    result=result.reshape(img.shape)
+    return result
+
+img=read_img("./IMG_20210314_075708.jpg")
+line_wdt=9
+blur_value=7
+total_colors=6
+
+edge_img=edge_detection(img,line_wdt,blur_value)
+img=color_quantisation(img,total_colors)
+blurred=cv2.bilateralFilter(img,d=7,sigmaColor=200,sigmaSpace=200)
+cartoon=cv2.bitwise_and(blurred,blurred,mask=edge_img)
+cv2.imwrite('cartoon.jpg',cartoon)
